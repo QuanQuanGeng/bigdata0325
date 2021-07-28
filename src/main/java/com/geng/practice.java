5a35aa57f314e949af99e6191fd85ed0393b8b0c
gqq <gqq@qq.com>
2021-07-28 18:50:56 +0800

    this is my second commit

--- src/main/java/com/geng/practice.java
+++ src/main/java/com/geng/practice.java
@@ -4,5 +4,7 @@ public class practice {
     public static void main(String[] args) {
         System.out.println("耿圈圈");
         System.out.println("耿俊豪");
+        System.out.println("Hello World");
+        System.out.println("tell me why");
     }
 }
